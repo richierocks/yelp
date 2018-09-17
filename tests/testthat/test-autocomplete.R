9a0e83ffe8184301d084619cab71ca8bfe09ad4c
Richard Cotton <richierocks@gmail.com>
2018-09-17 13:41:36 -0400

    tests

--- tests/testthat/test-autocomplete.R
+++ tests/testthat/test-autocomplete.R
@@ -1,9 +1,21 @@
 old_token <- set_token()
 
-test_results_have_correct_form(
-  autocomplete("del"),
-  c("id", "name"),
-  c("character", "character")
-)
+test_that(
+  "autocomplete() returns a list with 3 terms", {
+  actual <- autocomplete("del")
+  expect_type(actual, "list")
+  expect_length(actual, 3)
+  expect_equal(names(actual), c("terms", "businesses", "categories"))
+  expect_equal(colnames(actual$businesses), c("id", "name"))
+  expect_equal(
+    vapply(actual$businesses, class, character(1L), USE.NAMES = FALSE),
+    c("character", "character")
+  )
+  expect_equal(colnames(actual$categories), c("alias", "title"))
+  expect_equal(
+    vapply(actual$categories, class, character(1L), USE.NAMES = FALSE),
+    c("character", "character")
+  )
+})
 
 unset_token(old_token)
