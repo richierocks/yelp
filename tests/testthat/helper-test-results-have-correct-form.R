5a95308ae044e0c3249fb1a0d2f2b88d93ecf17a
Richard Cotton <richierocks@gmail.com>
2018-10-14 17:15:24 -0400

    more tests

--- tests/testthat/helper-test-results-have-correct-form.R
+++ tests/testthat/helper-test-results-have-correct-form.R
@@ -1,5 +1,5 @@
-test_results_have_correct_form <- function(actual, expected_column_names, expected_column_classes) {
-  fn_called <- deparse(substitute(actual)[[1]])
+test_results_have_correct_form <- function(actual, expected_column_names,
+  expected_column_classes, fn_called = deparse(substitute(actual)[[1]])) {
   test_description <- sprintf("%s() returns a data frame", fn_called)
   top_class <- function(x) class(x)[1]
   test_that(
